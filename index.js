811c6d8a847365bba3d3968b888ebece08a8878c
gabrielmorais4 <gabrielmoraisdev4@gmail.com>
2025-09-29 16:30:56 +0200

    :refacto: implement handling function and clean code

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-function foo() {
-    for (let i = 1; i <= 100; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz")
-        }
-        else if (i % 3 === 0) {
-            console.log("Fizz")
-        }
-        else if (i % 5 === 0) {
-            console.log("Buzz")
-        }
-        else {
-            console.log(i)
-        }
-    }
+const FIZZ_NUMBER = 3;
+const BUZZ_NUMBER = 5;
+const MAX_NUMBER = 100;
+
+function fizzBuzzValue(i) {
+  const word =
+    (i % FIZZ_NUMBER === 0 ? "Fizz" : "") +
+    (i % BUZZ_NUMBER === 0 ? "Buzz" : "");
+  console.log(word || i);
 }
 
-foo()
+function runFizzBuzz() {
+  for (let i = 1; i <= MAX_NUMBER; i++) {
+    fizzBuzzValue(i);
+  }
+}
+
+runFizzBuzz();
